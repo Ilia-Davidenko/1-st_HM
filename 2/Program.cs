4eda6a21da7751a4e42003e25a06e6f8a929af3d
Davidenko Ilia <Ilia_d@outlook.com>
2022-04-12 18:17:34 +0300

    2 задача

---
+++ 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+int x = new Random().Next(0, 999);
+WriteLine($"Первое число {x}");
+int y = new Random().Next(0, 999);
+WriteLine($"Второе число {y}");
+int z = new Random().Next(0, 999);
+WriteLine($"Третье число {z}");
+int max = x;
+
+if (y > max)
+{
+    max = y;
+}
+if (z > max)
+{
+    max = z;
+}
+WriteLine($"Максимальное число {max}");
