ef793925823605ff18ce6819345bb344f54aee67
Davidenko Ilia <Ilia_d@outlook.com>
2022-04-12 18:30:50 +0300

    3 Задача

---
+++ 3/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static System.Console;
+
+int x = new Random().Next(0, 9999);
+WriteLine(x);
+if (x%2 == 0)
+{
+    WriteLine("число четное");
+}
+else WriteLine("число НЕ четное");
