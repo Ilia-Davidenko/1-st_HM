eebaaaa0f6ed8b3fd3ae84f4cb240582f6f1bc60
Davidenko Ilia <Ilia_d@outlook.com>
2022-04-12 18:42:05 +0300

    4 задача

---
+++ 4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+WriteLine("Введите число");
+int x = Convert.ToInt32(ReadLine());
+int count = 1;
+
+while (count <= x)
+{
+    if (count % 2 == 0)
+    {
+        WriteLine(count);
+        count++;
+    }
+    else count++;
+}
