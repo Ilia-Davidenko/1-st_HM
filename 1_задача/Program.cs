1e9940ac6e2946e634b4924e4390dc2ca5cc3b83
Davidenko Ilia <Ilia_d@outlook.com>
2022-04-12 17:18:25 +0300

    1 задача

--- 1_задача/Program.cs
+++ 1_задача/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using static System.Console;
+
+WriteLine("Введите первое число");
+int x = Convert.ToInt32(ReadLine());
+WriteLine("Введите второе число");
+int y = Convert.ToInt32(ReadLine());
+
+if (x > y)
+{
+    WriteLine($"Большее число {x}, меньшее {y}");
+}
+else WriteLine($"Большее число {y}, меньшее {x}");
